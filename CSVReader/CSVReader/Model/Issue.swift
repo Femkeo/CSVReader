e7894f464ecb18108d38f028067e5df82d2baf10
Femke Offringa <foffringa@mobiquityinc.com>
2019-10-09 11:40:15 +0200

    created Issue Object and starting setup of Issue Reader

---
+++ CSVReader/CSVReader/Model/Issue.swift
@@ -0,0 +1,16 @@
+//
+//  Issue.swift
+//  CSVReader
+//
+//  Created by Femke Offringa on 09/10/2019.
+//  Copyright © 2019 Femke Offringa. All rights reserved.
+//
+
+import Foundation
+
+struct Issue {
+    let firstName: String = ""
+    let lastName: String = ""
+    let numberOfIssues: Int = 0
+    let birthDay : Date?
+}
---
+++ CSVReader/CSVReader/Network/CSVReader.swift
@@ -0,0 +1,13 @@
+//
+//  CSVReader.swift
+//  CSVReader
+//
+//  Created by Femke Offringa on 09/10/2019.
+//  Copyright © 2019 Femke Offringa. All rights reserved.
+//
+
+import Foundation
+
+class CSVReader{
+    //this class will read the CSV file
+}
